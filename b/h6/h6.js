242d0d26fde952a73cedeb6e7db1b36b0715d450
Nikhil Jain <nik.jain@gmail.com>
2024-02-20 00:29:34 +0530

    some testing

--- b/h6/h6.js
+++ b/h6/h6.js
@@ -1,9 +1,24 @@
-export default async (req, context) => {
-  const { city, country } = context.params;
 
-  return new Response(`You're visiting ${city} in ${country}!`);
-};
 
-export const config = {
-  path: "/travel-guide/:city/:country"
-};
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event, context) => {
+  console.log(context)
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify(subject),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
